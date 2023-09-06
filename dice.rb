05ae66942dbd624a1ab3edda28ed6c3d31907e6a
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-06 23:27:18 +0000

    other die added

--- dice.rb
+++ dice.rb
@@ -10,3 +10,35 @@ end
 get("/giraffe") do 
 "Hopefully this shows up"
 end
+get("/dice/2/6") do
+first_die = rand(1..6)
+second_die = rand(1..6)
+sum = first_die + second_die 
+
+outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+"<h1> 2d10 </h1>
+<p>#{outcome}</p>"
+end
+get("/dice/2/10") do
+  first_die = rand(1..10)
+second_die = rand(1..10)
+sum = first_die + second_die 
+
+outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+"<h1> 2d10 </h1>
+<p>#{outcome}</p>"
+end
+get("dice/5/4")do
+first_die = rand(1..4)
+second_die = rand(1..4)
+third_die = rand(1..4)
+fourth_die = rand(1..4)
+sum = first_die + second_die +third_die +fourth_die
+
+outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} for a total of #{sum}."
+
+"<h1> 5d4 </h1>
+<p>#{outcome}</p>"
+end
