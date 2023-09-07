bdf42c920f01dcdcc144bd53b7c62f747a42aca0
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-07 00:46:24 +0000

    adding other erbs

--- dice.rb
+++ dice.rb
@@ -19,8 +19,8 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die 
   
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  #erb(:two_six) 
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_six) 
   end
 get("/dice/2/10") do
   first_die = rand(1..10)
